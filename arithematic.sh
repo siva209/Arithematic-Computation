89ad04722e7d4ad211127e73e2455fb27b918651
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-12 14:29:04 +0530

    [added decending ]

--- arithematic.sh
+++ arithematic.sh
@@ -7,17 +7,31 @@ result2=$(($(($a*$b))+$c))
 result3=$(($c+$(($a/$b))))
 result4=$(($(($a%$b))+$c))
 declare -A arr
-j=1
-for i in $result1 $result2 $result3 $result4
-do
-	arr[$j]=$i
-	((j++))
-done
+arr[1]=$result1
+arr[2]=$result2
+arr[3]=$result3
+arr[4]=$result4
 echo ${arr[@]}
 echo ${!arr[@]}
-for ((i=0;i<=${#arr[@]};i++))
+for ((i=1;i<${#arr[@]};i++))
 do
   a[$i]=${arr[$i]}
 done
 echo ${a[@]}
-
+echo ${!a[@]}
+temp=0
+for ((i=0;i<=${#a[@]};i++))
+do
+	for (( j=0;j<=$((${#a[@]}-$i));j++))
+	do
+         echo ${a[j]}
+         echo ${a[j+1]}
+		if [[ ${a[$j]} < ${a[$(($j+1))]} ]]
+		then
+			temp=${a[$j]}
+			a[$j]=${a[$(($j+1))]}
+			a[$(($j+1))]=$temp
+		fi
+	done
+done
+echo ${a[@]}
