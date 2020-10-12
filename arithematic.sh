35f0741a33f43a9aa356b338f843c30bcfc8282c
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-12 13:28:28 +0530

    [added Dictionary]

--- arithematic.sh
+++ arithematic.sh
@@ -5,4 +5,13 @@ read -p "enter the number: " c
 result1=$(($a+$(($b*$c))))
 result2=$(($(($a*$b))+$c))
 result3=$(($c+$(($a/$b))))
-reslut4=$(($(($a%$b))+$c))
+result4=$(($(($a%$b))+$c))
+declare -A arr
+j=1
+for i in $result1 $result2 $result3 $result4
+do
+	arr[$j]=$i
+	((j++))
+done
+echo ${arr[@]}
+echo ${!arr[@]}
