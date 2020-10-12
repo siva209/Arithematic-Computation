ba5a5cf9139a1cad5c7aa6af206984250b6c7e5c
siva209 <sivanarayanayeruva209@gmail.com>
2020-10-12 13:14:46 +0530

    [added result 4]

--- arithematic.sh
+++ arithematic.sh
@@ -3,4 +3,6 @@ read -p "enter the number :" a
 read -p "enter the number: " b
 read -p "enter the number: " c
 result1=$(($a+$(($b*$c))))
-result2=$(($c+$(($a/$b))))
+result2=$(($(($a*$b))+$c))
+result3=$(($c+$(($a/$b))))
+
